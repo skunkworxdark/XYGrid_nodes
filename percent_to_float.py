7f16a4ae51c65e5601509cf700893b4278aca9dd
Jaid <6216144+Jaid@users.noreply.github.com>
2023-11-29 23:47:14 +0100

    Formatted percent_to_float.py to match images_to_grids.py
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- percent_to_float.py
+++ percent_to_float.py
@@ -1,18 +1,18 @@
 from invokeai.app.invocations.baseinvocation import (
-  BaseInvocation,
-  InputField,
-  invocation,
+	BaseInvocation,
+	InputField,
+	invocation,
 )
 from invokeai.app.invocations.primitives import FloatOutput
 
 def process(text: str) -> float:
-  text = text.strip()
-  if text.endswith('%'):
-    formattedText = text[:-1].strip()
-    number = float(formattedText) / 100
-  else:
-    number = float(text)
-  return number
+    text = text.strip()
+    if text.endswith('%'):
+        formattedText = text[:-1].strip()
+        number = float(formattedText) / 100
+    else:
+        number = float(text)
+    return number
 
 @invocation(
   'percent_to_float',
@@ -32,12 +32,12 @@ def process(text: str) -> float:
   version='1.0.0',
 )
 class PercentToFloatInvocation(BaseInvocation):
-  """Converts a string to a float. If the input ends with a percent sign, it will be rescaled accordingly."""
-  text: str = InputField(
-    title='Text',
-    description='Input text',
-  )
+    """Converts a string to a float. If the input ends with a percent sign, it will be rescaled accordingly."""
+    text: str = InputField(
+        title='Text',
+        description='Input text',
+    )
 
-  def invoke(self, context) -> FloatOutput:
-    output = process(self.text)
-    return FloatOutput(value=output)
+    def invoke(self, context) -> FloatOutput:
+        output = process(self.text)
+        return FloatOutput(value=output)
